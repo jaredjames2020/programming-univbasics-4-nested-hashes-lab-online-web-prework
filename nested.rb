dc76d8fe500e0d3d46933caf8468d036683e3af4
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-08-13 23:38:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -134,7 +134,7 @@ def adding_to_dennis
         }
      }
 
-  programmer_hash[:dennis_ritchier][:languages] << ["Assembly"]
+  programmer_hash[:dennis_ritchier][:languages[1]] = ["Assembly"]
   p programmer_hash
 
 end
